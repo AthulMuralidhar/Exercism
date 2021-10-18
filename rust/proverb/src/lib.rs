25b74832575f5f9262976af3fd5cd0422b6efbb0
Athul Muralidhar <athul.muralidhar@bricr.com>
2021-10-18 17:54:31 +0200

    old commit

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,15 +1,3 @@
-
-const TEMPLATE: [&str; 2] = [
-    "For want of a {} the {} was lost.",
-    "And all for the want of a {}."
-];
-
 pub fn build_proverb(list: &[&str]) -> String {
-    for word in list.iter() {
-        if word == list.last() {
-            return format!("And all for the want of a {}.", word);
-        } else {
-            return  format!("For want of a {} the {} was lost.", word, list.next());
-        }
-    }
- }
+    unimplemented!("build a proverb from this list of items: {:?}", list)
+}
